c28165c95d923e7d7d9bf7d2f574408de3e1159a
mohamed <lanjes991@gmail.com>
2025-12-16 21:40:50 +0300

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,20 @@
+
+import "./App.css";
+import Greeting from "./components/Greeting";
+import { Header } from  "./components/Header";
+
+
+
+
+function App() {
+  
+
+  return (
+    <>
+      <Greeting name="Mohamed" surname="Abdirahman" />
+      <Header />
+    </>
+  );
+}
+
+export default App;
---
+++ src/components/Header.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export const Header = () => {
+  return (
+    <div>
+        <h1>this is my first react tutorial</h1>
+    </div>
+  )
+}
