abdc2825a65b9e8537096cd406632b93cabb2f7b
mohamed <lanjes991@gmail.com>
2025-12-17 20:18:07 +0300

    i ahve added h2 and empty fragment

--- src/components/Greeting.jsx
+++ src/components/Greeting.jsx
@@ -1,5 +1,13 @@
 function Greeting(props) {
-  return <h1>Hello, {props.name} {props.surname}!</h1>;
+  return (
+    <>
+      <h1>
+        Hello, {props.name} {props.surname}!
+      </h1>
+      ;
+      <h1>This text wasnnot there</h1>
+    </>
+  ); 
 }
 
 export default Greeting;
